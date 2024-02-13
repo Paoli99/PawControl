af517b62f626fb8bbc3fff6883f87581c8757311
Paola Vilaseca <pao_anahi_8@hotmail.com>
2024-02-12 23:48:17 -0400

    user can now upload and update their profile picture

--- lib/firebase/firebase_auth/firebase_auth.dart
+++ lib/firebase/firebase_auth/firebase_auth.dart
@@ -6,6 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:pawcontrol/constants/constants.dart';
 
 
@@ -28,72 +29,33 @@ class FirebaseAuthenticator {
     }
   }
 
-    Future<bool> signUp(
-    File? profilePicture, // Cambiado a File? para manejar casos donde no se sube ninguna imagen
-    String firstName,
-    String lastName,
-    String phone,
-    String address,
-    String email,
-    String password,
-    String confirmPassword,
-    BuildContext context,
-  ) async {
-    try {
-      showLoaderDialog(context);
+  Future<bool> signUp(
+        String firtName, String lastName, String phone, String address, String email, String password, String confirmPassword, BuildContext context) async {
+      try {
+        showLoaderDialog(context);
 
-      String imageUrl = '';
+        await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
 
-      // Subir la imagen a Firebase Storage si se ha seleccionado una
-      if (profilePicture != null) {
-        imageUrl = await uploadImageToFirebaseStorage(context, profilePicture);
-      }
+        String userId = _auth.currentUser!.uid;
 
-      await _auth.createUserWithEmailAndPassword(email: email, password: password);
-
-      String userId = _auth.currentUser!.uid;
-
-      // Guardar la URL de la imagen en Firestore si se ha subido una
-      if (imageUrl.isNotEmpty) {
         await FirebaseFirestore.instance.collection('users').doc(userId).set({
-          'profilePicture': imageUrl,
-          'firstName': firstName,
+          'firstName': firtName,
           'lastName': lastName,
           'phone': phone,
           'address': address,
           'email': email,
         });
-      } else {
-        // Si no se ha subido una imagen, guardar solo los otros datos del usuario en Firestore
-        await FirebaseFirestore.instance.collection('users').doc(userId).set({
-          'firstName': firstName,
-          'lastName': lastName,
-          'phone': phone,
-          'address': address,
-          'email': email,
-        });
-      }
 
-      Navigator.of(context).pop();
-      return true;
-    } on FirebaseAuthException catch (error) {
-      Navigator.of(context).pop();
-      showMessage(context, error.code.toString());
-      return false;
+        Navigator.of(context).pop();
+        return true;
+      } on FirebaseAuthException catch(error) {
+        Navigator.of(context).pop();
+        showMessage(context, error.code.toString());
+        return false;
+      }
     }
-  }
 
-  Future<String> uploadImageToFirebaseStorage(BuildContext context, File imageFile) async {
-  try {
-    Reference storageReference = FirebaseStorage.instance.ref().child('profilePictures').child(imageFile.path);
-    UploadTask uploadTask = storageReference.putFile(imageFile);
-    TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
-    return await taskSnapshot.ref.getDownloadURL();
-  } catch (error) {
-    showMessage(context, 'Error al subir la imagen: $error');
-    return '';
-  }
-}
   
   void signOut() async {
     await _auth.signOut();
@@ -109,6 +71,6 @@ class FirebaseAuthenticator {
     }
 
   }
-
+  
 }
 
--- lib/screens/auth_ui/sign_up/signup.dart
+++ lib/screens/auth_ui/sign_up/signup.dart
@@ -27,274 +27,221 @@ class SignUp extends StatefulWidget {
   @override
   _SignUpState createState() => _SignUpState();
 }
-  class _SignUpState extends State<SignUp>{
+  class _SignUpState extends State<SignUp> {
+  bool _isPasswordVisible = true;
+  File? _image;
 
-    bool _isPasswordVisible = true; 
-    File? _image;
+  TextEditingController firtName = TextEditingController();
+  TextEditingController lastName = TextEditingController();
+  TextEditingController phone = TextEditingController();
+  TextEditingController address = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmPassword = TextEditingController();
 
-    TextEditingController firtName = TextEditingController();
-    TextEditingController lastName = TextEditingController();
-    TextEditingController phone = TextEditingController();
-    TextEditingController address = TextEditingController();
-    TextEditingController email = TextEditingController();
-    TextEditingController password = TextEditingController();
-    TextEditingController confirmPassword = TextEditingController();
-
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
           child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-                            
-            Container(
-              width: double.infinity, // Establece el ancho del contenedor al ancho total de la pantalla
-              height: 100,
-              decoration: BoxDecoration(
-                color: ColorsApp.white70,
-              ),
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.white70,
                   ),
-                  SizedBox(width: 10),
-                  Image.asset(
-                    AssetsImages.instance.logo,
-                    width: 60,
-                    height: 60,
-                    fit: BoxFit.fill,
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    "PAW CONTROL",
-                    style: TextsFont.tituloHeader
-                  ),
-                ],
-              ),
-            ),
-              Container(
-                //margin: EdgeInsets.only(top: 20),
-                width: 480, // set the width of the container
-                height: 650, // set the height of the container
-
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    
-                    TextInputFields(
-                      controller: firtName,
-                      hintText: 'Ingrese su nombre',
-                      prefixIcon: Icon(
-                        Icons.person_2_outlined,
-                        color: Colors.grey,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
                       ),
-                      backgroundColor: ColorsApp.white70,
-                    ),
-
-                     SizedBox(
-                      height: 15.0,
-                    ),
-
-                    TextInputFields(
-                      controller: lastName,
-                      hintText: 'Ingrese su apellido',
-                      prefixIcon: Icon(
-                        Icons.person_2_outlined,
-                        color: Colors.grey,
+                      SizedBox(width: 10),
+                      Image.asset(
+                        AssetsImages.instance.logo,
+                        width: 60,
+                        height: 60,
+                        fit: BoxFit.fill,
                       ),
-                      backgroundColor: ColorsApp.white70,
-                    ),
-                                        
-                     SizedBox(
-                      height: 15.0,
-                    ),
-
-                    TextFields(
-                      controller: phone,
-                      hintText: 'Ingrese su numero de teléfono',
+                      SizedBox(width: 10),
+                      Text(
+                        "PAW CONTROL",
+                        style: TextsFont.tituloHeader,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 480,
+                  height: 650,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 15.0),
+                      TextInputFields(
+                        controller: firtName,
+                        hintText: 'Ingrese su nombre',
+                        prefixIcon: Icon(
+                          Icons.person_2_outlined,
+                          color: Colors.grey,
+                        ),
+                        backgroundColor: Colors.white70,
+                      ),
+                      SizedBox(height: 15.0),
+                      TextInputFields(
+                        controller: lastName,
+                        hintText: 'Ingrese su apellido',
+                        prefixIcon: Icon(
+                          Icons.person_2_outlined,
+                          color: Colors.grey,
+                        ),
+                        backgroundColor: Colors.white70,
+                      ),
+                      SizedBox(height: 15.0),
+                      TextFields(
+                        controller: phone,
+                        hintText: 'Ingrese su número de teléfono',
                         prefixIcon: Icon(
                           Icons.phone_iphone_outlined,
                           color: Colors.grey,
                         ),
-                        backgroundColor: ColorsApp.white70,
+                        backgroundColor: Colors.white70,
                       ),
-                      
-
-                    SizedBox(
-                      height: 15.0,
-                    ),
-
-                    TextInputFields(
-                      controller: address,
-                      hintText: 'Ingrese su dirección',
-                      prefixIcon: Icon(
-                        Icons.other_houses_outlined,
-                        color: Colors.grey,
-                      ),
-                      backgroundColor: ColorsApp.white70,
-                    ),
-                                        
-                     SizedBox(
-                      height: 15.0,
-                    ),
-                    TextInputFields(
-                      controller: email,
-                      hintText: 'Ingrese su correo',
-                      prefixIcon: Icon(
-                        Icons.email_outlined,
-                        color: Colors.grey,
-                      ),
-                      backgroundColor: ColorsApp.white70,
-                    ),
-                                        
-                     SizedBox(
-                      height: 15.0,
-                    ),
-
-
-                    TextInputFields(
-                      controller: password,
-                      hintText: 'Ingrese su contraseña',
-                      obscureText: _isPasswordVisible,
-                      prefixIcon: Icon(
-                        Icons.lock_outlined,
-                        color: Colors.grey,
-                      ),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _isPasswordVisible
-                              ? Icons.visibility
-                              : Icons.visibility_off,
+                      SizedBox(height: 15.0),
+                      TextInputFields(
+                        controller: address,
+                        hintText: 'Ingrese su dirección',
+                        prefixIcon: Icon(
+                          Icons.other_houses_outlined,
                           color: Colors.grey,
                         ),
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordVisible = !_isPasswordVisible;
-                          });
-                        },
+                        backgroundColor: Colors.white70,
                       ),
-                      backgroundColor: ColorsApp.white70,
-                    ),
-                    
-                    SizedBox(
-                      height: 15.0,
-                    ),
-
-                    TextInputFields(
-                      controller: confirmPassword,
-                      hintText: 'Confirme su contraseña',
-                      obscureText: _isPasswordVisible,
-                      prefixIcon: Icon(
-                        Icons.lock_outlined,
-                        color: Colors.grey,
-                      ),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _isPasswordVisible
-                              ? Icons.visibility
-                              : Icons.visibility_off,
+                      SizedBox(height: 15.0),
+                      TextInputFields(
+                        controller: email,
+                        hintText: 'Ingrese su correo',
+                        prefixIcon: Icon(
+                          Icons.email_outlined,
                           color: Colors.grey,
                         ),
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordVisible = !_isPasswordVisible;
-                          });
-                        },
+                        backgroundColor: Colors.white70,
                       ),
-                      backgroundColor: ColorsApp.white70,
-                    ),
-                    SizedBox(
-                      height: 15.00,
-                    ),
-                    SecondaryButton(
-                      title: 'Seleccionar imagen de perfil', 
-                      onPressed: (){
-                      },
-                    ),
-                    SizedBox(
-                      height: 15.00,
-                    ),
-                    PrimaryButton(title: 'Registrame', onPressed: () async{
-                      bool isValidated =
-                        signUpValidation(context, 
-                        firtName.text,
-                        lastName.text,
-                        phone.text,
-                        address.text,
-                        email.text, 
-                        password.text,
-                        confirmPassword.text);
-                        if (isValidated) {
-                        bool isLogedin = await FirebaseAuthenticator.instance
-                          .signUp(
-                          _image,
-                          firtName.text,
-                          lastName.text,
-                          phone.text,
-                          address.text,
-                          email.text,
-                          password.text,
-                          confirmPassword.text,
-                          context);
-                        if (isLogedin) {
-                          Routes.instance.pushAndRemoveUntil(
-                          widget: const Home(),
-                          context: context,
+                      SizedBox(height: 15.0),
+                      TextInputFields(
+                        controller: password,
+                        hintText: 'Ingrese su contraseña',
+                        obscureText: _isPasswordVisible,
+                        prefixIcon: Icon(
+                          Icons.lock_outlined,
+                          color: Colors.grey,
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                            color: Colors.grey,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
+                          },
+                        ),
+                        backgroundColor: Colors.white70,
+                      ),
+                      SizedBox(height: 15.0),
+                      TextInputFields(
+                        controller: confirmPassword,
+                        hintText: 'Confirme su contraseña',
+                        obscureText: _isPasswordVisible,
+                        prefixIcon: Icon(
+                          Icons.lock_outlined,
+                          color: Colors.grey,
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                            color: Colors.grey,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
+                          },
+                        ),
+                        backgroundColor: Colors.white70,
+                      ),
+
+                      SizedBox(height: 15.0),
+                      PrimaryButton(
+                        title: 'Registrarme',
+                        onPressed: () async {
+                          bool isValidated = signUpValidation(
+                            context,
+                            firtName.text,
+                            lastName.text,
+                            phone.text,
+                            address.text,
+                            email.text,
+                            password.text,
+                            confirmPassword.text,
                           );
-                        }
-                      }
-                      },
-                    ),
-                    SizedBox(
-                      height: 5.00,
-                    ),
-                    SizedBox(height: 5.0),
-
-                    Padding(padding: EstilosBase.listPadding,
-                      child: RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                          text: '¿Ya tienes una cuenta? ',
-                          style: TextsFont.cuerpo,
-                          children: [
-                            TextSpan(
-                              text: '¡Inicia sesión!',
-                              style: TextsFont.link,
-                              recognizer: TapGestureRecognizer()
-                              ..onTap= () {
-                                  Routes.instance.push(
-                                    widget: SignUp(),
-                                    context: context,
-                                  );
-                                },
-                            )
-                          ]
-                        ),),
-                      )
-                  ],
-                  
+                          if (isValidated) {
+                            bool isLogedin = await FirebaseAuthenticator.instance.signUp(
+                              firtName.text,
+                              lastName.text,
+                              phone.text,
+                              address.text,
+                              email.text,
+                              password.text,
+                              confirmPassword.text,
+                              context,
+                            );
+                            if (isLogedin) {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => Home()),
+                              );
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(height: 5.0),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            text: '¿Ya tienes una cuenta? ',
+                            style: TextStyle(color: Colors.black),
+                            children: [
+                              TextSpan(
+                                text: '¡Inicia sesión!',
+                                style: TextStyle(color: Colors.blue),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    Navigator.pop(context);
+                                  },
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              
-
-              
-            ],),
+              ],
+            ),
+          ),
         ),
-        ),
-      
       ),
     );
   }
-  }
+}
